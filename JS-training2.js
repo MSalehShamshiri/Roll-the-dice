1063b6046526d18eb46bd313d0999d1f82eea6b1
Saleh Shamshiri <120913442+MSalehShamshiri@users.noreply.github.com>
2023-05-17 20:44:31 +0330

    Add files via upload

---
+++ JS-training2.js
@@ -0,0 +1,4 @@
+document.querySelector("ul").lastElementChild.innerHTML = "lksjeifojef";
+document.querySelector("ul li a").style.color = "red";
+document.querySelector(".btn").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
